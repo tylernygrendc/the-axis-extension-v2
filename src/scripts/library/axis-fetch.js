799f6efd482c2d4cb499fb2392ea95e5de08445b
Tyler Nygren <tylernygren@gmail.com>
2026-05-12 18:06:42 -0400

    fix login handling

--- src/scripts/library/axis-fetch.js
+++ src/scripts/library/axis-fetch.js
@@ -13,13 +13,18 @@ export async function frontOfficeFetch(resource = "", options = {}) {
     return {
       ...response,
       async json() {
-        try { return typeof this.body === "object" ? this.body : JSON.parse(this.body); } 
-        catch { return {}; }
+        try {
+          return typeof this.body === "object"
+            ? this.body
+            : JSON.parse(this.body);
+        } catch {
+          return {};
+        }
       },
       async text() {
         return String(this.body);
       },
-    }
+    };
   } catch (error) {
     console.error(error);
     return {
@@ -31,15 +36,28 @@ export async function frontOfficeFetch(resource = "", options = {}) {
 
 // perform multiple front office get requests form anywhere using bulk api
 export async function frontOfficeBulkFetch(requests = []) {
+  // modify options for bulk fetch
+  options = Object.assign(options, {
+    body: { requests: requests },
+    method: "POST",
+  });
+  // fetch normally via frontOfficeFetch
+  // ? frontOfficeFetch handles its own errors
+  return await frontOfficeFetch("https://axis.thejoint.com/rest/v11_24/bulk", {
+    method: "POST",
+    credentials: "include",
+    body: { requests: requests },
+  });
+}
+
+// login to back office from anywhere via app-background.js
+export async function backOfficeLogin(token, email, password, status) {
   try {
-    // modify options for bulk fetch
-    options = Object.assign(options, { body: { requests: requests }, method: "POST" });
-    // fetch normally via frontOfficeFetch
-    return await frontOfficeFetch("https://axis.thejoint.com/rest/v11_24/bulk", {
-      method: "POST",
-      credentials: "include",
-      body: { requests: requests }
-    });
+    const api = backOfficeAPI.login(token, email, password, status);
+    await frontOfficeLogin(email, password);
+    await fetch(api.fetchRequest, api.fetchOptions);
+    // TODO allow user to set default resource
+    window.open("https://backoffice.thejoint.com/pending-notes", "_self");
   } catch (error) {
     console.error(error);
   }
@@ -47,69 +65,114 @@ export async function frontOfficeBulkFetch(requests = []) {
 
 // login to front office from anywhere via app-background.js
 export async function frontOfficeLogin(email, password) {
-  const api = frontOfficeAPI.login(email, password);
-  let response = await frontOfficeFetch(api.fetchRequest, api.fetchOptions);
-  if(response.ok) {
-    response = await response.json();
-    // update auth tokens in chrome.storage
-    await contentStorage.set({
-      access_token: response.access_token,
-      download_token: response.download_token
-    }, "sync", toMS.s(response.expires_in));
-    await contentStorage.set({
-      refresh_token: response.refresh_token,
-    }, "sync", toMS.s(response.refresh_expires_in));
-    // avoid conflict with open front office tabs
-    await updateTokensInAllFrontOfficeTabs(response.access_token, response.refresh_token, response.download_token);
-  } else {
-    // there was a login error
-  }
-}
-
-export function frontOfficeRefresh() {
-  // get refresh token
-  let record = await contentStorage.get("refreshToken", "sync");
-  // attempt refresh
-  if (record.ok) {
-    const api = frontOfficeAPI.refresh(record.results?.refreshToken);
-    // TODO switch to api.fetchRequest
-    let response = await frontOfficeFetch(api.url, {
-      method: api.method,
-      body: api.body
-    });
-    if(response.ok) {
+  try {
+    const api = frontOfficeAPI.login(email, password);
+    let response = await frontOfficeFetch(api.fetchRequest, api.fetchOptions);
+    if (response.ok) {
       response = await response.json();
       // update auth tokens in chrome.storage
-      await contentStorage.set({
-        access_token: response.access_token,
-        download_token: response.download_token
-      }, "sync", toMS.s(response.expires_in));
-      await contentStorage.set({
-        refresh_token: response.refresh_token,
-      }, "sync", toMS.s(response.refresh_expires_in));
+      await contentStorage.set(
+        {
+          access_token: response.access_token,
+          download_token: response.download_token,
+        },
+        "sync",
+        toMS.s(response.expires_in),
+      );
+      await contentStorage.set(
+        {
+          refresh_token: response.refresh_token,
+        },
+        "sync",
+        toMS.s(response.refresh_expires_in),
+      );
       // avoid conflict with open front office tabs
-      await updateTokensInAllFrontOfficeTabs(response.access_token, response.refresh_token, response.download_token);
+      await updateTokensInAllFrontOfficeTabs(
+        response.access_token,
+        response.refresh_token,
+        response.download_token,
+      );
+      // TODO update carbon credentails
     } else {
-      // there was a login error
+      throw new Error(response.statusText);
     }
-  } else {
-    // new login is necessary
+    return { ok: true };
+  } catch (error) {
+    console.error(error);
+    return {
+      ok: false,
+      statusText: error,
+    };
   }
 }
 
-async function updateTokensInAllFrontOfficeTabs(oauthToken, refreshToken, downloadToken) {
+export async function frontOfficeRefresh() {
+  try {
+    // get refresh token
+    let record = await contentStorage.get("refreshToken", "sync");
+    // attempt refresh
+    if (typeof record?.refreshToken === "string") {
+      const api = frontOfficeAPI.refresh(record.refreshToken);
+      let response = await frontOfficeFetch(api.fetchRequest, api.fetchOptions);
+      if (response.ok) {
+        response = await response.json();
+        // update auth tokens in chrome.storage
+        await contentStorage.set(
+          {
+            access_token: response.access_token,
+            download_token: response.download_token,
+          },
+          "sync",
+          toMS.s(response.expires_in),
+        );
+        await contentStorage.set(
+          {
+            refresh_token: response.refresh_token,
+          },
+          "sync",
+          toMS.s(response.refresh_expires_in),
+        );
+        // avoid conflict with open front office tabs
+        await updateTokensInAllFrontOfficeTabs(
+          response.access_token,
+          response.refresh_token,
+          response.download_token,
+        );
+        return { ok: true };
+      } else {
+        throw new Error(response.statusText);
+      }
+    } else {
+      throw new Error("Refresh token does not exist in chrome.storage.sync.");
+    }
+  } catch (error) {
+    // console.info(error);
+    return {
+      ok: false,
+      statusText: error,
+    };
+  }
+}
+
+async function updateTokensInAllFrontOfficeTabs(
+  oauthToken,
+  refreshToken,
+  downloadToken,
+) {
   // find all front office contexts
-  const frontOfficeTabs = await chrome.tabs.query({ url: "https://axis.thejoint.com/*" });
+  const frontOfficeTabs = await chrome.tabs.query({
+    url: "https://axis.thejoint.com/*",
+  });
   if (frontOfficeTabs.length) {
     // wait for all open contexts to receive updated auth tokens
     await chrome.scripting.executeScript({
-      target: { tabIds: frontOfficeTabs.map(tab => tab.id) },
+      target: { tabIds: frontOfficeTabs.map((tab) => tab.id) },
       func: (oauthToken, refreshToken, downloadToken) => {
         localStorage.setItem("oauth_token", oauthToken);
         localStorage.setItem("refresh_token", refreshToken);
         localStorage.setItem("download_token", downloadToken);
       },
-      args: [oauthToken, refreshToken, downloadToken] // Passes tokenData into the 'token' parameter above
+      args: [oauthToken, refreshToken, downloadToken], // passes tokenData into the 'token' parameter above
     });
   }
   return true;
